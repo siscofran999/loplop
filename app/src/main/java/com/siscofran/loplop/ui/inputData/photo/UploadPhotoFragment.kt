d1202ab537360831c18f4dc96c8ea17eebe0dd46
fransisco <fransisco@tsmid.com>
2021-02-01 23:15:03 +0700

    upload photo setengah

--- app/src/main/java/com/siscofran/loplop/ui/inputData/photo/UploadPhotoFragment.kt
+++ app/src/main/java/com/siscofran/loplop/ui/inputData/photo/UploadPhotoFragment.kt
@@ -1,29 +1,59 @@
 package com.siscofran.loplop.ui.inputData.photo
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.siscofran.loplop.R
+import com.siscofran.loplop.databinding.FragmentUploadPhotoBinding
 
-class UploadPhotoFragment : Fragment() {
+class UploadPhotoFragment : Fragment(), View.OnClickListener {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    private var _binding: FragmentUploadPhotoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_photo, container, false)
+        _binding = FragmentUploadPhotoBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
-    companion object {
-//        fun newInstance(param1: String, param2: String) =
-//            UploadPhotoFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imgProfile.setOnClickListener(this)
+        binding.imgProfile2.setOnClickListener(this)
+        binding.imgProfile3.setOnClickListener(this)
+        binding.imgProfile4.setOnClickListener(this)
+        binding.include.btnNext.setOnClickListener(this)
+
+        binding.include.btnNext.isEnabled = false
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.img_profile, R.id.img_profile2, R.id.img_profile3, R.id.img_profile4 -> {
+                showDialog()
+            }
+            R.id.btn_next -> {
+
+            }
+        }
+    }
+
+    private fun showDialog() {
+
+        val bs = BottomSheetDialog(requireContext(), R.style.BottomSheetDialog)
+        val bsView = layoutInflater.inflate(R.layout.bs_upload_image, binding.root, false)
+        bs.setContentView(bsView)
+
+        bs.show()
     }
 }
