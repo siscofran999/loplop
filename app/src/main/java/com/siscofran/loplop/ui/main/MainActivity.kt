b45505409869db63a4326d22ae446cf18e4d996f
fransisco <fransisco@tsmid.com>
2021-03-13 15:01:33 +0700

    add header MainActivity

--- app/src/main/java/com/siscofran/loplop/ui/main/MainActivity.kt
+++ app/src/main/java/com/siscofran/loplop/ui/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.siscofran.loplop.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.siscofran.loplop.R
+import com.siscofran.loplop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
